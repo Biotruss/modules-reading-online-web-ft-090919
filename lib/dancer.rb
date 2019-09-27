72d0fd55fe5d4002e5b2f61982135bd344cfc0f0
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-27 15:22:29 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,12 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-class Dancer 
+class Dancer
+  
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
-  
-  extend FanceDance::ClassMethods 
-  include FancyDance::InstanceMethods
 end
